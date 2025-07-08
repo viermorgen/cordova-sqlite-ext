e8f6bf89226ab22bf5dd0bac3239286f9829ddfd
Julian Becker <jb@4morgen.de>
2025-07-08 08:26:57 +0200

    DBFile anpassen

--- src/android/io/sqlc/SQLitePlugin.java
+++ src/android/io/sqlc/SQLitePlugin.java
@@ -206,7 +206,8 @@ public class SQLitePlugin extends CordovaPlugin {
             // ASSUMPTION: no db (connection/handle) is already stored in the map
             // [should be true according to the code in DBRunner.run()]
 
-            File dbfile = this.cordova.getActivity().getDatabasePath(dbname);
+            // File dbfile = this.cordova.getActivity().getDatabasePath(dbname);
+            File dbfile = new File(this.cordova.getActivity().getExternalFilesDir(null), dbname);
 
             if (!dbfile.exists() && createFromResource) this.createFromResource(dbname, dbfile);
 
@@ -351,7 +352,8 @@ public class SQLitePlugin extends CordovaPlugin {
      * @return true if successful or false if an exception was encountered
      */
     private boolean deleteDatabaseNow(String dbname) {
-        File dbfile = this.cordova.getActivity().getDatabasePath(dbname);
+        // File dbfile = this.cordova.getActivity().getDatabasePath(dbname);
+        File dbfile = new File(this.cordova.getActivity().getExternalFilesDir(null), dbname);
 
         try {
             return cordova.getActivity().deleteDatabase(dbfile.getAbsolutePath());
